f079c15a7153c456e09a883d588e7ac41d02d837
Tomer Moran <tmoran_github@coveo.com>
2023-03-21 16:54:13 -0400

    Add Typescript type declarations

---
+++ types.d.ts
@@ -0,0 +1,78 @@
+declare module "gcs-browser-upload" {
+  type ChunkUploadData = {
+    totalBytes: number
+    uploadedBytes: number
+    chunkIndex: number
+    chunkLength: number
+  }
+
+  type UploadOptions = {
+    /** A unique ID for the upload */
+    id: string
+
+    /** GCS resumable URL */
+    url: string
+
+    /** The File to upload */
+    file: File
+
+    /**
+     * Chunk size must be a multiple of 262144
+     * @default 262144
+     */
+    chunkSize?: number
+
+    /**
+     * Storage mechanism used to persist chunk metadata
+     * @default window.localStorage
+     */
+    storage?: Storage
+
+    /**
+     * Content type of the file being uploaded
+     * @default 'text/plain'
+     */
+    contentType?: string
+
+    /** A function that will be called with progress information */
+    onChunkUpload?: (data: ChunkUploadData) => void
+  }
+  export default class Upload {
+    static errors: typeof UploadErrors
+
+    constructor(config: UploadOptions, allowSmallChunks?: boolean)
+
+    start(): Promise<void>
+    pause(): void
+    unpause(): void
+    cancel(): void
+  }
+
+  namespace UploadErrors {
+    declare class DifferentChunkError extends Error {
+      constructor(
+        chunkIndex: number,
+        originalChecksum: string,
+        newChecksum: string
+      )
+    }
+    declare class FileAlreadyUploadedError {
+      constructor(id: string, url: string)
+    }
+    declare class UrlNotFoundError {
+      constructor(url: string)
+    }
+    declare class UploadFailedError {
+      constructor(status: number)
+    }
+    declare class UnknownResponseError {
+      constructor(res: unknown)
+    }
+    declare class MissingOptionsError {}
+    declare class UploadIncompleteError {}
+    declare class InvalidChunkSizeError {
+      constructor(chunkSize: number)
+    }
+    declare class UploadAlreadyFinishedError {}
+  }
+}
